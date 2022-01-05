bb9e90c84e18d19366fe1eff1895766c1a53c391
meanii <anilchauhanxda@gmail.com>
2022-01-05 08:17:53 +0530

    using env varibales insteads of hard coding in auth services

--- auth/server.js
+++ auth/server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+const PASSWORD = process.env.PASSWORD || 'secretkey';
+
 app.use(express.urlencoded({ extended: true }))
 
 app.post('/auth', function (req, res) {
   /* This server is only available to nginx */ 
-  if (req.body.key) {
-      var streamkey = req.body.key;
-  }
+  if (req.body.key) var streamkey = req.body.key;
   
   /* You can make a database of user instead */
-  if (streamkey === "secretkey") return res.status(200).send();
+  if (streamkey === PASSWORD) return res.status(200).send();
 
   /* Reject the stream */
   res.status(403).send();
 });
 
-app.listen(8000, function() {
-    console.log("Listening on port 8000")
+app.listen(PORT, function() {
+    console.log(`Listening on port ${PORT}`)
 })
