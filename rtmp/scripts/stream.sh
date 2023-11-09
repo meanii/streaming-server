93e47c1e979f72a4e99410019199bca3dd149e62
anil <anilchauhanxda@gmail.com>
2023-11-10 00:23:20 +0530

    rewrite rtmp-server

---
+++ rtmp/scripts/stream.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+app=$1
+name=$2
+
+ffmpeg -i rtmp://localhost:1935/$app/$name \
+    -c:a libfdk_aac -b:a 128k -c:v libx264 -b:v 2500k -f flv -g 30 -r 30 -s 1280x720 -preset superfast -profile:v baseline rtmp://localhost:1935/hls/${name}_720p2628kbs \
+    -c:a libfdk_aac -b:a 128k -c:v libx264 -b:v 1000k -f flv -g 30 -r 30 -s 854x480 -preset superfast -profile:v baseline rtmp://localhost:1935/hls/${name}_480p1128kbs \
+    -c:a libfdk_aac -b:a 128k -vn -f flv rtmp://localhost:1935/hls/"${name}audio"
